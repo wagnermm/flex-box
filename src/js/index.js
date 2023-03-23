4119d5dc55c9cd4840a2b5de281a6d0821973963
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-03-23 10:39:52 -0300

    configurando style/scss/webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+import '../scss/style.scss'
 
 function titleComponent() {
     const elemH1 = document.createElement('h1');
     elemH1.innerHTML = 'Hello, world!';
+    elemH1.classList.add('h1')
     return elemH1
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path')
+const HtmlWebpack = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|c|sc)ss$/i, 
+                use: [
+                    MiniCssExtract.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpack({
+            template: './src/index.html',
+            filename: 'index.html',     
+        }),
+        new MiniCssExtract({
+            filename: 'style.css'
+        })
+    ]
 }
