198ca90c64213f3f71a3f267313dfa4677e28689
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-03-24 12:55:04 -0300

    iniciando com flexbox

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,3 @@ import '../scss/style.scss'
 
 const elemRoot = document.getElementById('root')
 elemRoot.classList.add('container')
-
-function titleComponent() {
-    const elemH1 = document.createElement('h1');
-    elemH1.innerHTML = 'Simulator';
-    elemH1.classList.add('h1');
-    return elemH1
-}
-
-document.getElementById('root').appendChild(titleComponent());
