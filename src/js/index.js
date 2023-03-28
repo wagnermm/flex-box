ad990cdb72a1d1b45a54e7a17e4b7afb67082615
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-03-28 15:49:36 -0300

    adiciona animacoes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 import '../scss/style.scss'
+import imageBuilding from '../assets/building.jpg'
+import imageNotebook from '../assets/notebook.jpg'
 
 const elemRoot = document.getElementById('root')
 elemRoot.classList.add('container')
+
+const elemetImageBuilding = new Image()
+elemetImageBuilding.src = imageBuilding
+
+const elemetImageNotebook = new Image()
+elemetImageNotebook.src = imageNotebook
+
+document.querySelector('#building').appendChild(elemetImageBuilding)
+document.querySelector('#notebook').appendChild(elemetImageNotebook)
