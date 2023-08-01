94b6a5da5c8912037216667f2ee4a4930f5ee6d4
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-08-01 15:52:19 -0300

    ajustes

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import '../scss/style.scss'
 
 var openMenu = document.querySelector('.btn-menu').addEventListener('click', function() {
-    document.querySelector('nav-mobile').style.visibility="visible";
+    document.querySelector('.nav-mobile').style.visibility="visible";
 });
 
-var closeMenu = document.querySelector('.nav-mobile').addEventListener('click', function() {
-    document.querySelector('nav-mobile').style.visibility="hidden";
+var closeMenu = document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.nav-mobile').style.visibility="hidden";
 });
