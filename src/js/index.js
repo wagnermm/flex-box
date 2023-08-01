e4ed3d28d8102b6eba50ec4c9124340b190b0b9f
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-08-01 09:38:36 -0300

    ajustando header com flexbox

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import '../scss/style.scss'
+
+var openMenu = document.querySelector('.btn-menu').addEventListener('click', function() {
+    document.querySelector('nav-mobile').style.visibility="visible";
+});
+
+var closeMenu = document.querySelector('.nav-mobile').addEventListener('click', function() {
+    document.querySelector('nav-mobile').style.visibility="hidden";
+});
